5474d71356c90b216737cdcd025d80639c1a8c14
alfabouh <forgemail07@mail.ru>
2024-08-31 13:10:32 +0300

    upd

--- src/main/java/jgems_api/Main.java
+++ src/main/java/jgems_api/Main.java
@@ -1,6 +1,7 @@
 package jgems_api;
 
 import javagems3d.engine.JavaGemsLauncher;
+import javagems3d.toolbox.ToolBoxLauncher;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/jgems_api/test/TestGame.java
+++ src/main/java/jgems_api/test/TestGame.java
@@ -9,28 +9,6 @@
  *
  */
 
-/*
- * *
- *  * @author alfabouh
- *  * @since 2024
- *  * @link https://github.com/alfabouh/JavaGems3D
- *  *
- *  * This software is provided 'as-is', without any express or implied warranty.
- *  * In no event will the authors be held liable for any damages arising from the use of this software.
- *
- */
-
-/*
- * *
- *  * @author alfabouh
- *  * @since 2024
- *  * @link https://github.com/alfabouh/JavaGems3D
- *  *
- *  * This software is provided 'as-is', without any express or implied warranty.
- *  * In no event will the authors be held liable for any damages arising from the use of this software.
- *
- */
-
 package jgems_api.test;
 
 import javagems3d.engine.JGems3D;
