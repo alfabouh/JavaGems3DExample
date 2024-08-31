322fabba78a9f6446f80b8421679cfc77e27fe21
alfabouh <forgemail07@mail.ru>
2024-08-31 12:36:35 +0300

    Initial commit

---
+++ src/main/java/jgems_api/Main.java
@@ -0,0 +1,9 @@
+package jgems_api;
+
+import javagems3d.engine.JavaGemsLauncher;
+
+public class Main {
+    public static void main(String[] args) {
+        JavaGemsLauncher.launch(args);
+    }
+}
